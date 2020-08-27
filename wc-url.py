ecaba76f0d1345218cc5f4bdb6716e8dab1da3c3
Ramond The Incredible Lowe <raymondclowe@gmail.com>
2020-08-28 07:07:01 +0800

    arg parse

--- wc-url.py
+++ wc-url.py
@@ -1,5 +1,28 @@
-import pandas as pd;
+import argparse
+from validators import url as urlvalidator
 
-print("test")
+parser = argparse.ArgumentParser()
+
+parser.add_argument("urlorfile", help="URL to be processed or text file with list of URLs, one per line")
+
+parser.add_argument("-t","--tags",default='body', help="list of tags to include, defaults to body.  example --tags p,h1,h2,li")
+
+
+args = parser.parse_args()
+
+urlorfile = args.urlorfile
+
+tags = args.tags
+
+if urlvalidator(urlorfile):
+    urls = [urlorfile]
+else:
+    with open(urlorfile) as f:
+        urls = f.readlines()
+
+print(urls)
+
+
+print("--end--")
 
 
