05518e5cd21a993623e9c2947c08642cf0c7cf72
Raymond Lowe <raymond@wlmedia.com>
2020-08-28 12:36:31 +0800

    convert parser to lxml

--- wc-url.py
+++ wc-url.py
@@ -6,49 +6,55 @@ from bs4 import BeautifulSoup
 import re
 from validators import url as urlvalidator
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("urlorfile", help="URL to be processed or text file with list of URLs, one per line")
+def main():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("-t","--tags",default='body', help="list of tags to include, defaults to body.  example --tags p,h1,h2,li")
+    parser.add_argument("urlorfile", help="URL to be processed or text file with list of URLs, one per line")
+
+    parser.add_argument("-t","--tags",default='body', help="list of tags to include, defaults to body.  example --tags p,h1,h2,li")
 
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-urlorfile = args.urlorfile
+    urlorfile = args.urlorfile
 
-tags = args.tags
+    tags = args.tags
 
-if urlvalidator(urlorfile):
-    urls = [urlorfile]
-else:
-    if os.path.isfile(urlorfile):
-        with open(urlorfile) as f:
-            urls = f.readlines()
-        urls = [x.strip() for x in urls] 
+    if urlvalidator(urlorfile):
+        urls = [urlorfile]
     else:
-        Exception('Not URL or file')
+        if os.path.isfile(urlorfile):
+            with open(urlorfile) as f:
+                urls = f.readlines()
+            urls = [x.strip() for x in urls] 
+        else:
+            Exception('Not URL or file')
 
-requests_cache.install_cache('wc-url-cache')
-taglist = tags.split(",")
+    requests_cache.install_cache('wc-url-cache')
+    taglist = tags.split(",")
 
-for url in urls:    
-    page = requests.get(url)        
-    soup = BeautifulSoup(page.text, features="html.parser")
+    for url in urls:    
+        page = requests.get(url)        
+        # soup = BeautifulSoup(page.text, features="html.parser")
+        soup = BeautifulSoup(page.text, features="lxml")
 
-    text = ''
-    for tag in taglist:
-        contentslist = soup.find_all(tag)
-        for elem in contentslist:
-            text = text + ' ' + elem.getText()
-    
-    wordlist = re.findall(r"[\w']+", text)
+        text = ''
+        for tag in taglist:
+            contentslist = soup.find_all(tag)
+            for elem in contentslist:
+                text = text + ' ' + elem.getText()
+        
+        wordlist = re.findall(r"[\w']+", text)
 
-    wc = len(wordlist)
+        wc = len(wordlist)
 
-    print('"' + url + '",', wc)
+        print('"' + url + '",', wc)
 
 
-print("--end--")
+    print("--end--")
+
+main()
+
 
 
