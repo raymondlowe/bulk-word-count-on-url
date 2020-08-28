cc78f02777a26dac0b8d2a876750d74138c7a4ac
Raymond Lowe <raymond@wlmedia.com>
2020-08-28 10:54:24 +0800

    first working version

--- README.md
+++ README.md
@@ -6,11 +6,40 @@ By default checks the <body>, but can be also used to check <p> or <article> or 
 
 Simplistic definition of word is "string separated by space".
 
-installation:
+##installation:
 
+```
 pip3 install -r requirements.txt
+```
 
-usage:
+##usage:
 
+```
 python3 wc-url.py [url|file-of-urls.txt] [-t|--tag body|p|p,h1,li|<other valid list of elements>]
+```
+
+##Example:
+
+```
+python3 wc-url.py example.txt --tags p 
+```
+
+Outputs:
+
+```
+"http://example.com", 26
+"https://www.example.com", 26
+--end--
+```
+
+Because there are 26 words within the paragraphs of the page.
+
+
+## Note:
+
+Includes caching, delete the file wc-url-cache.sqlite to clear the cache.
+
+###Caution:
+
+It will double count if you choose tags that may be inside one another. e.g. --tags body,p
 
--- wc-url.py
+++ wc-url.py
@@ -1,5 +1,9 @@
 import os.path
 import argparse
+import requests
+import requests_cache
+from bs4 import BeautifulSoup
+import re
 from validators import url as urlvalidator
 
 parser = argparse.ArgumentParser()
@@ -25,7 +29,24 @@ else:
     else:
         Exception('Not URL or file')
 
-print(urls)
+requests_cache.install_cache('wc-url-cache')
+taglist = tags.split(",")
+
+for url in urls:    
+    page = requests.get(url)        
+    soup = BeautifulSoup(page.text, features="html.parser")
+
+    text = ''
+    for tag in taglist:
+        contentslist = soup.find_all(tag)
+        for elem in contentslist:
+            text = text + ' ' + elem.getText()
+    
+    wordlist = re.findall(r"[\w']+", text)
+
+    wc = len(wordlist)
+
+    print('"' + url + '",', wc)
 
 
 print("--end--")
