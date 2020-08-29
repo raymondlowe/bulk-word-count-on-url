9fd16d21ad5d618c8c6bb10d7e40abacf55c1daa
raymondclowe <68897994+raymondclowe@users.noreply.github.com>
2020-08-29 12:51:13 +0800

    Get up to date (#1)
    * check file existance
    
    * cleanup, readme
    
    * first working version
    
    * Clean up git ignore
    
    * README.me
    
    * MD formatting
    
    * readme
    
    * convert parser to lxml
    
    Co-authored-by: raymondlowe <49975086+raymondlowe@users.noreply.github.com>
    Co-authored-by: Raymond Lowe <raymond@wlmedia.com>

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # bulk-word-count-on-url
-# bulk-word-count-on-url
-# bulk-word-count-on-url
+
+Check how many "Words" there are on a url or list of urls.
+
+By default checks the body gag, but can be also used to check p or article or any other item that may more accurately reflect what the actual content is.
+
+Word separation done using regex search so handles spaces and punctuation.
+
+## installation:
+
+```
+git clone https://github.com/raymondlowe/bulk-word-count-on-url.git
+cd bulk-word-count-on-url
+pip3 install -r requirements.txt
+```
+
+## usage:
+
+```
+python3 wc-url.py [url|file-of-urls.txt] [-t|--tag body|p|p,h1,li|<other valid list of elements>]
+```
+
+## Example:
+
+```
+python3 wc-url.py example.txt --tags p 
+```
+
+Outputs:
+
+```
+"http://example.com", 26
+"https://www.example.com", 26
+--end--
+```
+
+Because there are 26 words within the paragraphs of the page.
+
+
+## Note:
+
+Includes caching, delete the file wc-url-cache.sqlite to clear the cache.
+
+### Caution:
+
+It will double count if you choose tags that may be inside one another. e.g. --tags body,p
+
--- wc-url.py
+++ wc-url.py
@@ -1,29 +1,60 @@
+import os.path
 import argparse
+import requests
+import requests_cache
+from bs4 import BeautifulSoup
+import re
 from validators import url as urlvalidator
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("urlorfile", help="URL to be processed or text file with list of URLs, one per line")
+def main():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("-t","--tags",default='body', help="list of tags to include, defaults to body.  example --tags p,h1,h2,li")
+    parser.add_argument("urlorfile", help="URL to be processed or text file with list of URLs, one per line")
 
-
-args = parser.parse_args()
-
-urlorfile = args.urlorfile
-
-tags = args.tags
-
-if urlvalidator(urlorfile):
-    urls = [urlorfile]
-else:
-    with open(urlorfile) as f:
-        urls = f.readlines()
-    urls = [x.strip() for x in urls] 
-
-print(urls)
+    parser.add_argument("-t","--tags",default='body', help="list of tags to include, defaults to body.  example --tags p,h1,h2,li")
 
 
-print("--end--")
+    args = parser.parse_args()
+
+    urlorfile = args.urlorfile
+
+    tags = args.tags
+
+    if urlvalidator(urlorfile):
+        urls = [urlorfile]
+    else:
+        if os.path.isfile(urlorfile):
+            with open(urlorfile) as f:
+                urls = f.readlines()
+            urls = [x.strip() for x in urls] 
+        else:
+            Exception('Not URL or file')
+
+    requests_cache.install_cache('wc-url-cache')
+    taglist = tags.split(",")
+
+    for url in urls:    
+        page = requests.get(url)        
+        # soup = BeautifulSoup(page.text, features="html.parser")
+        soup = BeautifulSoup(page.text, features="lxml")
+
+        text = ''
+        for tag in taglist:
+            contentslist = soup.find_all(tag)
+            for elem in contentslist:
+                text = text + ' ' + elem.getText()
+        
+        wordlist = re.findall(r"[\w']+", text)
+
+        wc = len(wordlist)
+
+        print('"' + url + '",', wc)
+
+
+    print("--end--")
+
+main()
+
 
 
